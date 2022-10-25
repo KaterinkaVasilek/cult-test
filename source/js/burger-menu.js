e0f0c05c4f6608ce6b23b7115290078a552719ff
Katerinka <e.kushner@bk.ru>
2022-10-25 14:55:58 +0300

    init: new project

---
+++ source/js/burger-menu.js
@@ -0,0 +1,38 @@
+//burger-menu
+const toggleButton = document.querySelector('.toggle-menu');
+const nav = document.querySelector(".nav");
+const content = document.querySelector('.nav__content');
+const overlay = document.querySelector('.nav__overlay');
+const contentItem = document.querySelectorAll('.nav__item');
+
+export {toggleButton};
+
+if(document.querySelector('.burger-menu')) {
+    toggleButton.addEventListener("click", showMenu);
+
+    function showMenu() {
+        nav.classList.toggle('toggle');
+        overlay.classList.toggle('overlay-visible');
+        content.classList.toggle('nav__content--open');
+
+        document.addEventListener('click', event => {
+            const el = event.target;
+
+            contentItem.forEach(el => {
+                el.addEventListener("click", () => {
+                    closeMenu();
+                });
+            });
+        
+            if(el === overlay) {
+                closeMenu();
+            }
+        });
+    };
+
+    function closeMenu() {
+        nav.classList.remove('toggle');
+        overlay.classList.remove('overlay-visible');
+        content.classList.remove('nav__content--open');
+    }
+}
